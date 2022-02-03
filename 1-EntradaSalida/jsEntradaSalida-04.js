969c5980dcd80aceeea6bf972f70e8d91bf040d0
Lucas <lucasmateo4444@gmail.com>
2022-02-03 18:33:23 -0300

    Entrega del cuearto ejercicio

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -1,9 +1,13 @@
-/* 
+/* Trabajo de Acuña Lucas*/
+/* Ejercicio Numero 4*/
+/*
 Debemos lograr tomar un dato por 'PROMPT' 
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
-{
-	alert("ok");
-}
+{  
+	let nombre;
+	nombre = prompt("ingrese su nombre");
+	document.getElementById("txtIdNombre").value = nombre;
 
+}
