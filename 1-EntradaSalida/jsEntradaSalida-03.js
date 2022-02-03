3e01b0a80b97c18c11a5fb351b622bce89fe7865
Lucas <lucasmateo4444@gmail.com>
2022-02-03 17:43:33 -0300

    Entregando el ejercicio 3

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -1,12 +1,12 @@
-/*
-Debemos lograr tomar un dato por 'ID'
+/* Trabajo de Acuña Lucas*/
+/*Trabajo numero 3*/
+/*Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
-{
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+{   /*textIdNombre */
+	let nombreIngresado;
+	nombreIngresado= document.getElementById("textIdNombre").value;
+	alert(nombreIngresado);
 
 }
-
 
