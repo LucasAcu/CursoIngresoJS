b5c3f62f96bf372e72bfd2cd673ed0c7d5e3a474
Lucas <lucasmateo4444@gmail.com>
2022-02-04 17:01:06 -0300

    entrega del ejercicio 5 y 6

--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -1,8 +1,26 @@
+/* Trabajo de Acuña Lucas*/
+/* Trabajo numero 5*/
 /*
 Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	alert("ok");
+
+	let nombreIngrsado;
+	let edadIngresada;
+	let mensajeMostrar;
+
+
+	nombreIngresado = document.getElementById("txtIdNombre").value;
+	edadIngresada = document.getElementById("txtIdEdad").value;
+
+
+	mensajeMostrar = "usted se llama " + nombreIngresado + " y tiene " + edadIngresada + " edad";
+
+
+
+	alert(mensajeMostrar);
+
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -1,9 +1,26 @@
+//Trabajo de Acuña Lucas
+//ejercicio numero 6
 /*
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	let numero1;
+	let numero2;
+	let resultado;
+	let mensaje;
+
+	numero1 = document.getElementById("txtIdNumeroUno").value;
+	numero2 = document.getElementById("txtIdNumeroDos").value;
+
+	numero1 = parseInt(numero1);
+	numero2 = parseInt(numero2);
+	resultado = numero1 + numero2;
+	mensaje = "el resultado es " + resultado;
+
+
+
+	alert(mensaje);
 }
 
