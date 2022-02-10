3c52d576d86f1cb95cd31eccc2ae43200e5bd828
Lucas <lucasmateo4444@gmail.com>
2022-02-10 18:26:16 -0300

    entrega de trabajos

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -1,3 +1,4 @@
+// tp numero 1 de Acuña Lucas
 /*1.	Para el departamento de facturación:
 A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
@@ -5,13 +6,67 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	let precio1;
+	let precio2;
+	let precio3;
+	let sumar;
+	let promedio;
+	let precioFinal;
+
+	precio1 = document.getElementById("txtIdPrecioUno").value;
+	precio2 = document.getElementById("txtIdPrecioDos").value;
+	precio3 = document.getElementById("txtIdPrecioTres").value;
+
+	precio1 = parseInt(precio1);
+	precio2 = parseInt(precio2);
+	precio3 = parseInt(precio3);
+
+	resultado = (precio1 + precio2) + precio3;
+
+	mensaje = "El resultado es " + resultado;
+
+	alert(mensaje)
+
+
 }
 function Promedio () 
 {
-	
+	let numero1;
+	let numero2;
+	let numero3;
+	let suma;
+	let promedio;
+	let mensaje;
+
+	numero1 = parseInt(document.getElementById("txtIdPrecioUno").value);
+	numero2 = parseInt(document.getElementById("txtIdPrecioDos").value);
+	numero3 = parseInt(document.getElementById("txtIdPrecioTres").value);
+
+	suma = numero1 + numero2 + numero3;
+	promedio = suma / 3;
+	mensaje = "El resultado es " + promedio;
+
+	alert(mensaje);
+
 }
 function PrecioFinal () 
 {
-	
+	let precio1;
+	let precio2;
+	let precio3;
+	let precioFinal;
+	let iva;
+	let resultado; 
+
+	precio1 = parseInt(document.getElementById("txtIdPrecioUno").value);
+	precio2 = parseInt(document.getElementById("txtIdPrecioDos").value);
+	precio3 = parseInt(document.getElementById("txtIdPrecioTres").value);
+
+	iva = (precio1 + precio2 + precio3)*21/100;
+	precioFinal= iva;
+	resultado = "el precio final es " + precioFinal;
+	alert(resultado);
+
+
 }
+
--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -1,3 +1,4 @@
+//´TP 2 De Acuña Lucas
 /*2.	Para el departamento de Construcción:
 A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el ancho de un terreno rectangular y se debe alambra con tres hilos de alambre.
 B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
@@ -6,13 +7,40 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+	let rectangulo;
+	let largo;
+	let ancho;
+	let alambre;
+
+	largo = parseFloat(document.getElementById("txtIdLargo").value);
+	ancho = parseFloat(document.getElementById("txtIdAncho").value);
+
+	rectangulo = (largo + ancho)*2;
+	alambre = rectangulo *3;
+	mensaje = "la cantidad de almbre para el rectangulo es " + alambre;
+
+	alert(mensaje);
 
 }
 function Circulo () 
 {
-	
+	let circulo;
+	let radio;
+
+	radio = parseFloat(document.getElementById("txtIdRadio").value);
+
+	circulo = (2 * 3.14 * radio)*3;
+	alert("la cantidad de alambre para el circulo es " + circulo);
 }
 function Materiales () 
 {
-	
+	let materiales;
+	let ancho
+	let resultado
+	largo = parseFloat(document.getElementById("txtIdLargo").value)
+	ancho = parseFloat(document.getElementById("txtIdAncho").value)
+	superficie = largo + ancho;
+	resultado = superficie *3
+
+	alert(resultado)
 }
